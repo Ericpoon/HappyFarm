e62a0f94d6c3db59d32dd0a8a5070594dde3b9c2
Eric Poon <ericpoon510@gmail.com>
2017-01-17 23:37:40 +0800

    not much

--- routers/databaseRouter.js
+++ routers/databaseRouter.js
@@ -5,14 +5,20 @@ var database = require(__modeldir + '/database.js');
 
 // 127.0.0.1/db/test
 router.get('/test', function (req, res) {
-    console.log('Client connecting to database');
-    res.send('Database connected.');
+    // console.log('Client connecting to database');
+    // res.send('Database connected.');
+    function completion(data) {
+        res.send(data);
+    }
+
+    database.getLatestData('Eric', completion);
 });
 
 router.get('/getuserinfo', function (req, res) {
     function completion(data) {
         res.send(data)
     }
+
     database.getUserInfo('Eric', completion);
 });
 
@@ -20,6 +26,7 @@ router.get('/getsensordata', function (req, res) {
     function completion(data) {
         res.send(data);
     }
+
     database.getSensorData('Eric', completion);
 });
 router.get('/getlatestdata', function (req, res) {
